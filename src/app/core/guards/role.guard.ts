38d235e9a7dc153e7e39d3867a20b5e755ee46e9
Djurre <djurreaikema@gmail.com>
2025-08-06 15:22:16 +0200

    Basic HasRole guard

---
+++ src/app/core/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from '../../features/auth/data-access/auth.service';
+
+export function hasRole(allowedRoles: string[]): CanActivateFn {
+  return () => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    const user = authService.user();
+
+    if (!user) {
+      router.navigate(['/auth/login']);
+      return false;
+    }
+
+    if (allowedRoles.includes(user.role)) {
+      return true;
+    }
+
+    router.navigate(['/dashboard']);
+    return false;
+  }
+}
